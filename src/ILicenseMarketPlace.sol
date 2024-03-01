8b6c13e93f03643d9b9588d5cff76fb98ecef2f4
John Wu <me@johnswu.net>
2024-03-01 08:07:39 -0800

    Checkpoint

--- src/ILicenseMarketPlace.sol
+++ src/ILicenseMarketPlace.sol
@@ -3,22 +3,24 @@ pragma solidity ^0.8.23;
 
 interface ILicenseMarketPlace {
 
-	struct LicenseStatistics {
+	struct LicenseMetadata {
         uint256 totalSupply;
         uint256 numDerivatives;
+		uint256 licenseId;
     } // The number of supply and derivatives. 
 
 	// 	Registers an ipAsset that is registered on storyProtocol to the marketplace
-	function registerIpAsset(address storyProtocolIpId, uint256 setFee) external view returns (address);
+	// function registerIpAsset(address storyProtocolIpId, uint256 setFee) external view returns (address);
+	function registerIpAsset(address storyProtocolIpId) external returns (uint256);
 	
 	// Allows a user to buy a key for a specific world based on it's ip address
-	function buyKey(address buyer, uint256 storyProtocolIpId, uint256 amount) external view returns (address);
+	function buyKey(address sourceIpAssetAddress, address targeIpAssetAddress, uint256 amount) external payable;
 
 	// Allows a user to sell their key for a specific world.
-	function sellKey(address seller, uint256 storyProtocolIpId, uint256 amount) external view returns (address);
+	function sellKey(address sourceIpAssetAddress, address targeIpAssetAddress, uint256 amount) external payable;
 
-	function claimRoyalties(uint256 owner, uint256 storyProtocolIpId, uint256 tokenAmount) external view returns (address);
+	// function claimRoyalties(uint256 owner, uint256 storyProtocolIpId, uint256 tokenAmount) external view returns (address);
 
-	function distributeRoyalties(uint256 owner, uint256 storyProtocolIpId, uint256 tokenAmount) external view returns (address);
+	// function distributeRoyalties(uint256 owner, uint256 storyProtocolIpId, uint256 tokenAmount) external view returns (address);
 }
 
--- src/LicenseMarketPlace.sol
+++ src/LicenseMarketPlace.sol
@@ -4,141 +4,297 @@ pragma solidity ^0.8.23;
 import {IP} from "@story-protocol/protocol-core/contracts/lib/IP.sol";
 import {IPAssetRegistry} from "@story-protocol/protocol-core/contracts/registries/IPAssetRegistry.sol";
 import {IPResolver} from "@story-protocol/protocol-core/contracts/resolvers/IPResolver.sol";
-import {LicenseRegistry} from "@story-protocol/protocol-core/contracts/registries/LicenseRegistry.sol";
+import {ILicenseRegistry} from "@story-protocol/protocol-core/contracts/interfaces/registries/ILicenseRegistry.sol";
 import {ILicenseMarketPlace} from "./ILicenseMarketPlace.sol";
-
+import {IERC6551Account} from "erc6551/interfaces/IERC6551Account.sol";
+import {IIPAccount} from "@story-protocol/protocol-core/contracts/interfaces/IIPAccount.sol";
+import {StoryProtocolGateway} from "@story-protocol/protocol-periphery/contracts/StoryProtocolGateway.sol";
 
 contract LicenseMarketPlace is ILicenseMarketPlace {
     address public immutable NFT;
     address public immutable IP_RESOLVER;
     IPAssetRegistry public immutable IPA_REGISTRY;
-    LicenseRegistry public immutable LICENSE_REGISTRY;
-    // StoryProtocolGateway public SPG;
+    ILicenseRegistry public immutable LICENSE_REGISTRY;
+    uint256 public immutable POLICY_ID;
 
-    event Trade(address trader, address subject, bool isBuy, uint256 shareAmount, uint256 ethAmount, uint256 protocolEthAmount, uint256 subjectEthAmount, uint256 supply);
-    
+    event Trade(
+        address trader,
+        address subject,
+        bool isBuy,
+        uint256 shareAmount,
+        uint256 ethAmount,
+        uint256 protocolEthAmount,
+        uint256 subjectEthAmount,
+        uint256 supply
+    );
+
     address public protocolFeeDestination;
     uint256 public protocolFeePercent;
     uint256 public subjectFeePercent;
 
-    // We will need to have a mapping of the IP id to 
+    // We will need to have a mapping of the IP id to
 
     // SharesSubject => (Holder => Balance)
     mapping(address => mapping(address => uint256)) public sharesBalance;
 
     // SharesSubject => Supply
-    mapping(address => LicenseStatistics) public sharesStatistics;
+    mapping(address => LicenseMetadata) public sharesMetadata;
+
+    mapping(uint256 => address) public licenseIdToAddress;
 
     constructor(
         address licenseRegistryAddress,
         address ipAssetRegistry,
         address resolver,
-        address nft
+        address nft,
+        uint256 policyId
     ) {
-        LicenseRegistry = LicenseRegistry(licenseRegistryAddress);
+        LICENSE_REGISTRY = ILicenseRegistry(licenseRegistryAddress);
         IPA_REGISTRY = IPAssetRegistry(ipAssetRegistry);
         IP_RESOLVER = resolver;
         NFT = nft;
+        POLICY_ID = policyId;
+    }
+
+    function _registerIpAsset(
+        address storyProtocolIpId
+    ) internal returns (uint256) {
+        // Construct a license with the policy id
+        require(
+            storyProtocolIpId != address(0) &&
+                sharesBalance[storyProtocolIpId][storyProtocolIpId] <= 0,
+            "Already Registered"
+        );
+        uint256 licenseId = LICENSE_REGISTRY.mintLicense(
+            POLICY_ID,
+            storyProtocolIpId,
+            true,
+            1,
+            storyProtocolIpId
+        );
+        sharesBalance[storyProtocolIpId][storyProtocolIpId] = 1;
+        LicenseMetadata memory licenseMetadata = LicenseMetadata({
+            totalSupply: 1,
+            numDerivatives: 0,
+            licenseId: licenseId
+        });
+        sharesMetadata[storyProtocolIpId] = licenseMetadata;
+        return licenseId;
     }
 
     function registerIpAsset(
-        string memory ipName,
-        uint256 tokenId
-    ) external returns (address) {
-        bytes memory metadata = abi.encode(
-            IP.MetadataV1({
-                name: ipName,
-                hash: "",
-                registrationDate: uint64(block.timestamp),
-                registrant: msg.sender,
-                uri: ""
-            })
-        );
+        address storyProtocolIpId
+    ) external returns (uint256) {
+        return _registerIpAsset(storyProtocolIpId);
+    }
+
+    function _generateMetadata(
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId,
+        address registrant,
+        string calldata ip_name,
+        string calldata url
+    ) internal view returns (bytes memory) {
+        bytes32 hash = keccak256(abi.encode(chainId, tokenContract, tokenId));
         return
-            IPA_REGISTRY.register(
-                block.chainid,
-                NFT,
-                tokenId,
-                IP_RESOLVER,
-                true,
-                metadata
+            abi.encode(
+                IP.MetadataV1({
+                    name: ip_name,
+                    hash: hash,
+                    registrationDate: uint64(block.timestamp),
+                    registrant: registrant,
+                    uri: url
+                })
             );
     }
 
+    function registerExistingNFT(
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId,
+        string calldata ip_name,
+        string calldata url
+    ) external returns (uint256) {
+        address nftAccountAddr = IPA_REGISTRY.register(
+            chainId,
+            tokenContract,
+            tokenId,
+            msg.sender,
+            true,
+            _generateMetadata(
+                chainId,
+                tokenContract,
+                tokenId,
+                msg.sender,
+                ip_name,
+                url
+            )
+        );
+        return _registerIpAsset(nftAccountAddr);
+    }
+
+    function registerNewNFT() {}
+
+    function _verifyOwner(
+        address ownerAddr,
+        address nftAcctAddr
+    ) internal view returns (bool) {
+        IIPAccount ipAccount = IIPAccount(payable(nftAcctAddr));
+        address queriedOwner = ipAccount.owner();
+        return queriedOwner == ownerAddr;
+    }
+
+    /**
+     * Allows a user to buy a key from the marketplace.
+     * @param sourceIpAssetAddress The address of the IPAsset's license that you want to buy. It's required that you own the address
+     * @param targeIpAssetAddress The address of the IPAsset that you want to receive.
+     * @param amount The amount of the key to buy.
+     */
     function buyKey(
-        address ipAssetAddress,
+        address sourceIpAssetAddress,
+        address targeIpAssetAddress,
         uint256 amount
     ) public payable {
-        uint256 supply = sharesStatistics[ipAssetAddress];
-        require(supply > 0 || ipAssetAddress == msg.sender, "Only the shares' subject can buy the first share");
-        uint256 price = getPrice(supply, amount);
-        uint256 protocolFee = price * protocolFeePercent / 1 ether;
-        uint256 subjectFee = price * subjectFeePercent / 1 ether;
-        require(msg.value >= price + protocolFee + subjectFee, "Insufficient payment");
-        sharesBalance[ipAssetAddress][msg.sender] = sharesBalance[ipAssetAddress][msg.sender] + amount;
-        sharesStatistics[ipAssetAddress] = supply + amount;
-        emit Trade(msg.sender, ipAssetAddress, true, amount, price, protocolFee, subjectFee, supply + amount);
+        // function implementation goes here
+        uint256 supply = sharesBalance[sourceIpAssetAddress][
+            targeIpAssetAddress
+        ];
+        require(
+            supply > 0 || _verifyOwner(msg.sender, sourceIpAssetAddress),
+            "Only the IPAccount owner can buy the first share"
+        );
+
+        uint256 price = getPrice(
+            supply,
+            sharesMetadata[sourceIpAssetAddress].numDerivatives,
+            amount
+        );
+        uint256 protocolFee = (price * protocolFeePercent) / 1 ether;
+        uint256 subjectFee = (price * subjectFeePercent) / 1 ether;
+        require(
+            msg.value >= price + protocolFee + subjectFee,
+            "Insufficient payment"
+        );
+        sharesBalance[sourceIpAssetAddress][targeIpAssetAddress] =
+            sharesBalance[sourceIpAssetAddress][targeIpAssetAddress] +
+            amount;
+        sharesMetadata[sourceIpAssetAddress].totalSupply = supply + amount;
+        emit Trade(
+            targeIpAssetAddress,
+            sourceIpAssetAddress,
+            true,
+            amount,
+            price,
+            protocolFee,
+            subjectFee,
+            supply + amount
+        );
         (bool success1, ) = protocolFeeDestination.call{value: protocolFee}("");
-        (bool success2, ) = ipAssetAddress.call{value: subjectFee}("");
+        (bool success2, ) = sourceIpAssetAddress.call{value: subjectFee}("");
         require(success1 && success2, "Unable to send funds");
 
-        LicenseRegistry.mintLicense(
-
+        LICENSE_REGISTRY.mintLicense(
+            POLICY_ID,
+            sourceIpAssetAddress,
+            true,
+            amount,
+            targeIpAssetAddress
         );
     }
 
     // So youre NFT can own items as well as you
     function sellKey(
-        address ipAssetAddress,
+        address sourceIpAssetAddress,
+        address targeIpAssetAddress,
         uint256 amount
     ) public payable {
-        uint256 supply = sharesStatistics[ipAssetAddress];
+        uint256 supply = sharesMetadata[sourceIpAssetAddress].totalSupply;
         require(supply > amount, "Cannot sell the last share");
-        uint256 price = getPrice(supply - amount, amount);
-        uint256 protocolFee = price * protocolFeePercent / 1 ether;
-        uint256 subjectFee = price * subjectFeePercent / 1 ether;
-        require(sharesBalance[ipAssetAddress][msg.sender] >= amount, "Insufficient shares");
-        sharesBalance[ipAssetAddress][msg.sender] = sharesBalance[ipAssetAddress][msg.sender] - amount;
-        sharesStatistics[ipAssetAddress] = supply - amount;
-        emit Trade(msg.sender, ipAssetAddress, false, amount, price, protocolFee, subjectFee, supply - amount);
-        (bool success1, ) = msg.sender.call{value: price - protocolFee - subjectFee}("");
-        (bool success2, ) = protocolFeeDestination.call{value: protocolFee}("");
-        (bool success3, ) = ipAssetAddress.call{value: subjectFee}("");
-        require(success1 && success2 && success3, "Unable to send funds");
 
-        LicenseRegistry.burnLicenses(
-            msg.sender,
-            ipAssetAddress,
-            1,
-            ROYATY_CONTEXT,
-            MINTING_FEE,
-            MINTING_FEE_TOKNE
+        uint256 price = getPrice(
+            supply,
+            sharesMetadata[sourceIpAssetAddress].numDerivatives,
+            amount
         );
+        uint256 protocolFee = (price * protocolFeePercent) / 1 ether;
+        uint256 subjectFee = (price * subjectFeePercent) / 1 ether;
+
+        IIPAccount ipAccount = IIPAccount(payable(sourceIpAssetAddress));
+        address owner = ipAccount.owner();
+        require(
+            supply > 0 || _verifyOwner(msg.sender, targeIpAssetAddress),
+            "Only the IPAccount owner can buy the first share"
+        );
+
+        require(
+            sharesBalance[sourceIpAssetAddress][targeIpAssetAddress] >= amount,
+            "Insufficient shares"
+        );
+        sharesBalance[sourceIpAssetAddress][targeIpAssetAddress] =
+            sharesBalance[sourceIpAssetAddress][targeIpAssetAddress] -
+            amount;
+        sharesMetadata[sourceIpAssetAddress].totalSupply = supply - amount;
+        emit Trade(
+            targeIpAssetAddress,
+            sourceIpAssetAddress,
+            false,
+            amount,
+            price,
+            protocolFee,
+            subjectFee,
+            supply - amount
+        );
+        (bool success1, ) = msg.sender.call{
+            value: price - protocolFee - subjectFee
+        }("");
+        (bool success2, ) = protocolFeeDestination.call{value: protocolFee}("");
+        (bool success3, ) = targeIpAssetAddress.call{value: subjectFee}("");
+        require(success1 && success2 && success3, "Unable to send funds");
+        for (uint256 i = 0; i < amount; i++) {
+            uint256[] memory licenses_to_burn = new uint256[](1); // fixed-size array with values
+            licenses_to_burn[0] = sharesMetadata[sourceIpAssetAddress]
+                .licenseId;
+            LICENSE_REGISTRY.burnLicenses(
+                targeIpAssetAddress,
+                licenses_to_burn
+            );
+        }
     }
 
-    function getPrice(LicenseStatistics calldata supply, uint256 amount) public pure returns (uint256) {
-        uint256 sum1 = supply == 0 ? 0 : (supply - 1 )* (supply) * (2 * (supply - 1) + 1) / 6;
-        uint256 sum2 = supply == 0 && amount == 1 ? 0 : (supply - 1 + amount) * (supply + amount) * (2 * (supply - 1 + amount) + 1) / 6;
+    function getPrice(
+        uint256 totalCirculating,
+        uint256 numBurned,
+        uint256 amount
+    ) public pure returns (uint256) {
+        uint256 totalSupplyAndBurned = totalCirculating + numBurned;
+        uint256 sum1 = totalSupplyAndBurned == 0
+            ? 0
+            : ((totalSupplyAndBurned - 1) *
+                (totalSupplyAndBurned) *
+                (2 * (totalSupplyAndBurned - 1) + 1)) / 6;
+        uint256 sum2 = totalSupplyAndBurned == 0 && amount == 1
+            ? 0
+            : ((totalSupplyAndBurned - 1 + amount) *
+                (totalSupplyAndBurned + amount) *
+                (2 * (totalSupplyAndBurned - 1 + amount) + 1)) / 6;
         uint256 summation = sum2 - sum1;
-        return summation * 1 ether / 16000;
+        return (summation * 1 ether) / 16000;
     }
 
+    // function claimRoyalties(
+    //     address owner,
+    //     uint256 storyProtocolIpId,
+    //     uint256 tokenAmount
+    // ) external view returns (address) {
+    //     return address(owner);
+    // }
 
-
-
-    function claimRoyalties(
-        address owner,
-        uint256 storyProtocolIpId,
-        uint256 tokenAmount
-    ) external view returns (address) {
-        return address(owner);
-    }
-
-    function distributeRoyalties(
-        address owner,
-        uint256 storyProtocolIpId,
-        uint256 tokenAmount
-    ) external view returns (address) {
-        return address(owner);
-    }
+    // function distributeRoyalties(
+    //     address owner,
+    //     uint256 storyProtocolIpId,
+    //     uint256 tokenAmount
+    // ) external view returns (address) {
+    //     return address(owner);
+    // }
 }

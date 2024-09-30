da10c4981992e31499c3b2d7c48a17d5095bc012
0xShikhar <shikhar@iiitmanipur.ac.in>
2024-10-01 03:59:13 +0800

    refactor: update contracts to latest solidity version based on story protocol core contracts

--- test/IPARegistrar.t.sol
+++ test/IPARegistrar.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.23;
+pragma solidity ^0.8.26;
 
 import { Test } from "forge-std/Test.sol";
 import { IPAssetRegistry } from "@storyprotocol/core/registries/IPAssetRegistry.sol";
